8670285d560f21e1d719f2f9e55b0a76e444eb04
Albert Manyà <albertmp@eml.cc>
2014-11-14 12:14:51 +0100

    Add support for multiple AWS accounts

--- ec2ls.py
+++ ec2ls.py
@@ -32,22 +32,35 @@ def main(wf):
     parser.add_argument('--set-access-key-id', dest='aws_access_key_id', nargs='?', default=None)
     parser.add_argument('--set-secret-access-key', dest='aws_secret_access_key', nargs='?', default=None)
     parser.add_argument('--set-region', dest='region', nargs='?', default=None)
+    parser.add_argument('--set-active-account', dest='active_account', nargs='?', default=None)
     parser.add_argument('query', nargs='?', default=None)
     args = parser.parse_args(wf.args)
 
+    if args.active_account:
+        wf.settings['active_account'] = args.active_account
+        return 0
+
+    account = wf.settings.get('active_account', 'default')
+
+    if not account in wf.settings:
+        wf.settings['account'] = {}
+
     if args.aws_access_key_id:
-        wf.settings['aws_access_key_id'] = args.aws_access_key_id
+        wf.settings.setdefault(account, {})['aws_access_key_id'] = args.aws_access_key_id
+        wf.settings.save()
         return 0
 
     if args.aws_secret_access_key:
-        wf.settings['aws_secret_access_key'] = args.aws_secret_access_key
+        wf.settings.setdefault(account, {})['aws_secret_access_key'] = args.aws_secret_access_key
+        wf.settings.save()
         return 0
 
     if args.region:
-        wf.settings['region'] = args.region
+        wf.settings.setdefault(account, {})['region'] = args.region
+        wf.settings.save()
         return 0
 
-    aws_access_key_id = wf.settings.get('aws_access_key_id', None)
+    aws_access_key_id = wf.settings[account].get('aws_access_key_id', None)
 
     if not aws_access_key_id:
         wf.add_item('No aws_access_key_id set.',
@@ -57,7 +70,7 @@ def main(wf):
         wf.send_feedback()
         return 0
 
-    aws_secret_access_key = wf.settings.get('aws_secret_access_key', None)
+    aws_secret_access_key = wf.settings[account].get('aws_secret_access_key', None)
 
     if not aws_secret_access_key:
         wf.add_item('No aws_secret_access_key set.',
@@ -67,7 +80,7 @@ def main(wf):
         wf.send_feedback()
         return 0
 
-    region = wf.settings.get('region', None)
+    region = wf.settings[account].get('region', None)
 
     if not region:
         wf.add_item('No region set.',
@@ -82,7 +95,7 @@ def main(wf):
     def wrapper():
         return get_recent_instances(aws_access_key_id, aws_secret_access_key, region)
 
-    instances = wf.cached_data('instances', wrapper, max_age=10)
+    instances = wf.cached_data('instances-%s' % account, wrapper, max_age=10)
 
     if query:
         instances = wf.filter(query, instances, key=search_key_for_instance)
