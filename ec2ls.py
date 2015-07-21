88566def0cc8089e6788ebf32f5d672037a7ec50
Albert Manya <albertmp@eml.cc>
2015-07-21 16:29:04 +0200

    Use profile stored by aws cli instead of credentials

--- ec2ls.py
+++ ec2ls.py
@@ -6,10 +6,9 @@ import sys
 import boto.ec2
 from workflow import Workflow, ICON_NETWORK, ICON_WARNING
 
-def get_recent_instances(aws_access_key_id, aws_secret_access_key, region):
+def get_recent_instances(region, profile_name):
     conn = boto.ec2.connect_to_region(region,
-                                      aws_access_key_id=aws_access_key_id,
-                                      aws_secret_access_key=aws_secret_access_key)
+                                      profile_name=profile_name)
     reservations = conn.get_all_reservations()
 
     instances = []
@@ -29,29 +28,18 @@ def get_recent_instances(aws_access_key_id, aws_secret_access_key, region):
 
 def main(wf):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--set-access-key-id', dest='aws_access_key_id', nargs='?', default=None)
-    parser.add_argument('--set-secret-access-key', dest='aws_secret_access_key', nargs='?', default=None)
+    parser.add_argument('--profile-name', dest='profile_name', nargs='?', default=None)
     parser.add_argument('--set-region', dest='region', nargs='?', default=None)
-    parser.add_argument('--set-active-account', dest='active_account', nargs='?', default=None)
     parser.add_argument('query', nargs='?', default=None)
     args = parser.parse_args(wf.args)
-
-    if args.active_account:
-        wf.settings['active_account'] = args.active_account
-        return 0
 
     account = wf.settings.get('active_account', 'default')
 
     if not account in wf.settings:
         wf.settings['account'] = {}
 
-    if args.aws_access_key_id:
-        wf.settings.setdefault(account, {})['aws_access_key_id'] = args.aws_access_key_id
-        wf.settings.save()
-        return 0
-
-    if args.aws_secret_access_key:
-        wf.settings.setdefault(account, {})['aws_secret_access_key'] = args.aws_secret_access_key
+    if args.profile_name:
+        wf.settings.setdefault(account, {})['profile_name'] = args.profile_name
         wf.settings.save()
         return 0
 
@@ -61,39 +49,14 @@ def main(wf):
         return 0
 
     aws_access_key_id = wf.settings[account].get('aws_access_key_id', None)
+    profile_name = wf.settings[account].get('profile_name', 'default')
 
-    if not aws_access_key_id:
-        wf.add_item('No aws_access_key_id set.',
-                    'Please use awssetaccesskey to set your access key.',
-                    valid = False,
-                    icon=ICON_WARNING)
-        wf.send_feedback()
-        return 0
-
-    aws_secret_access_key = wf.settings[account].get('aws_secret_access_key', None)
-
-    if not aws_secret_access_key:
-        wf.add_item('No aws_secret_access_key set.',
-                    'Please use awssetsecretkey to set your secret key.',
-                    valid = False,
-                    icon=ICON_WARNING)
-        wf.send_feedback()
-        return 0
-
-    region = wf.settings[account].get('region', None)
-
-    if not region:
-        wf.add_item('No region set.',
-                    'Please use awssetregion to set your region.',
-                    valid = False,
-                    icon=ICON_WARNING)
-        wf.send_feedback()
-        return 0
+    region = wf.settings[account].get('region', 'eu-west-1')
 
     query = args.query
 
     def wrapper():
-        return get_recent_instances(aws_access_key_id, aws_secret_access_key, region)
+        return get_recent_instances(region, profile_name)
 
     instances = wf.cached_data('instances-%s' % account, wrapper, max_age=10)
 
